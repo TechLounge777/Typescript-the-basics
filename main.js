c795fde13c8888d4a8617747ae0295f7fe352d55
TechLounge777 <msblanche777@gmail.com>
2021-04-17 19:10:55 +0200

    variable types

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
 "use strict";
 exports.__esModule = true;
-var message = "welcome back";
-console.log(message);
+// let message = "welcome back";
+// console.log(message);
+var isBeginner = true;
+var total = 1000;
+var name = 'Alice';
+var sentence = "My names is " + name + "\n    I am a beginner in TypeScript";
+console.log(sentence);
+var n = null;
+var u = undefined;
+var list1 = [1, 2, 3];
+var list2 = [1, 2, 3];
+var person1 = ['Tom', 55];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+var randomValue = 10;
+randomValue = 'Tom';
+randomValue = true;
--- main.ts
+++ main.ts
@@ -1,5 +1,44 @@
-export {}
+ export {}
 
-let message = "welcome back";
-console.log(message);
+// let message = "welcome back";
+// console.log(message);
+
+let isBeginner: boolean = true;
+let total: number = 1000;
+let name: string = 'Alice';
+
+let sentence: string = `My names is ${name}
+    I am a beginner in TypeScript`; 
+
+    console.log(sentence);
+
+let n:null = null;
+let u:undefined = undefined;
+
+let list1:number[] = [1, 2, 3];
+let list2:Array<number> = [1, 2, 3];
+
+let person1: [string, number] = ['Tom', 55];
+
+enum Color {Red, Green, Blue};
+let c: Color = Color.Green;
+console.log(c);
+
+let randomValue: any = 10;
+randomValue = 'Tom';
+randomValue = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
